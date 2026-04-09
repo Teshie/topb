4beafa3c174904f02d28d1fe2c5b946254ab077e
Teshie <teshomeaychiluhem@gmail.com>
2026-04-09 14:22:55 +0300

    fix

--- telegram/main.go
+++ telegram/main.go
@@ -148,7 +148,7 @@ type depositSession struct {
 
 /* ===================== Config ===================== */
 var (
-	APIBase               = getenvDefault("API_BASE", "https://henb.teshie.dev")
+	APIBase               = getenvDefault("API_BASE", "https://topb.tabia.site")
 	AdminSupergroup int64 = -1002877017597
 
 	PayeeName  = "Henok"
@@ -3301,7 +3301,7 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	botToken := mustEnv("BOT_TOKEN")
-	miniAppURL := getenvDefault("MINI_APP_URL", "https://henb.teshie.dev")
+	miniAppURL := getenvDefault("MINI_APP_URL", "https://topb.tabia.site")
 
 	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
